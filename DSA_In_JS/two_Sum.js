b3bd16f023565b2f7fbdc9f471664078d18aeee7
Kartik Negi <97402674+Kartik2negi@users.noreply.github.com>
2025-04-01 00:00:02 +0530

    two sum

---
+++ DSA_In_JS/two_Sum.js
@@ -0,0 +1,20 @@
+// two sum
+
+let nums = [2,7,11,15];
+let target = 9
+
+function twoSum(arr,t){
+    let seen = new Map();
+    
+    for(let i=0; i< arr.length; i++){
+        let comp = t - arr[i];
+        
+        if(seen.has(comp)){
+            return [seen.get(comp), i]
+        }
+        seen.set(arr[i],i);
+    }
+    return pair;
+}
+
+console.log(twoSum(nums,target))
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 # Arrays 
 1. Remove Duplicates from Array - https://github.com/Kartik2negi/DSA/blob/main/DSA_In_JS/Remove_Duplicates_from_Array.js
+2. Two Sum
