fd20db15cf33ec1512ab8dc04dc90601026afd68
Kartik Negi <97402674+Kartik2negi@users.noreply.github.com>
2025-04-05 23:33:57 +0530

    memoization

---
+++ JS_coding_Qs/memoization.js
@@ -0,0 +1,44 @@
+// Write a memoization function ?
+
+
+function add(a,b){
+    return a+b;
+}
+
+function sum(obj){
+    return obj.a + obj.b + obj.c;
+}
+
+function memo(fn){
+    let cache = new Map();
+    
+    function seralize(val){
+        if(typeof val === "object" && val !== null){
+            let sortedkey = Object.keys(val).sort();
+            return `{${sortedkey.map((key) => `"${key}" : ${JSON.stringify(val[key])}` ).join(",")}}`
+        }
+        return JSON.stringify(val);
+    }
+    
+    return function(...args){
+        let key = args.map(seralize).join("|");
+        if(cache.has(key)){
+            console.log("cache....!!");
+            return cache.get(key)
+        }
+        let res = fn.apply(this,args);
+        cache.set(key,res);
+        return res;
+    }
+}
+
+let res = memo(sum);
+console.log(res({a:1,b:2,c:3}))
+console.log(res({c:3,b:2,a:1}))
+console.log(res({a:1,b:2,c:3}))
+
+let result = memo(add);
+
+// console.log(result(2,2))
+// console.log(result(3,3))
+// console.log(result(2,2))
