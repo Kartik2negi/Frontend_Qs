aa6251f77782b6dc28322c01762b72e02eb248be
Kartik Negi <97402674+Kartik2negi@users.noreply.github.com>
2026-05-27 08:32:38 -0700

    XOR solution

--- Blind_75/03_Missing_Number.js
+++ Blind_75/03_Missing_Number.js
@@ -42,6 +42,74 @@ function findMissingNumberInSequence(numbers) {
     return expectedSum - actualSum;
 }
 
-console.log(findMissingNumberInSequence([1,3,0]))
+console.log(findMissingNumberInSequence([1, 3, 0]))
 console.log(findMissingNumberInSequence([1]))
-console.log(findMissingNumberInSequence([3,0,4,2,1]))
+console.log(findMissingNumberInSequence([3, 0, 4, 2, 1]))
+
+
+// Solution 2
+
+// ## XOR Solution — Notes
+
+// ### Pattern
+// * Bit Manipulation (XOR)
+
+// ### XOR Properties
+// a ^ a = 0
+// a ^ 0 = a
+
+// * Same numbers cancel each other
+// * Order does not matter in XOR
+
+
+// ### Approach
+// 1. XOR all numbers from `0 → n`
+// 2. XOR all array elements
+// 3. Matching numbers cancel out
+// 4. Remaining value is missing number
+
+// ### Key Insight
+// * Every duplicate pair becomes `0`
+// * Only missing number remains at end
+
+// ### Important Line
+// xor ^= i;
+
+// Equivalent to:
+// xor = xor ^ i;
+
+// ### Example
+// 0 ^ 1 ^ 2 ^ 3 ^ 3 ^ 0 ^ 1
+
+// After cancellation:
+// 2
+
+// ### Time Complexity
+// O(n)
+
+// ### Space Complexity
+// O(1)
+
+
+function findMissingNumberInSequence(numbers) {
+
+    const n = numbers.length;
+
+    let xor = 0;
+
+    // XOR all numbers from 0 → n
+    for (let i = 0; i <= n; i++) {
+        xor ^= i;
+    }
+    console.log(xor)
+    // XOR array elements
+    for (const num of numbers) {
+        xor ^= num;
+    }
+
+    return xor;
+}
+
+console.log(findMissingNumberInSequence([1, 3, 0]))
+// console.log(findMissingNumberInSequence([1]))
+// console.log(findMissingNumberInSequence([3,0,4,2,1]))
